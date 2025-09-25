a2a122f360a5c3d2ed04baf7fdab6dce90262c85
Ayushcodekid <ayush0101works@gmail.com>
2025-09-25 23:18:39 +0530

    url changed

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://warrantyit-task.onrender.com",
+    origin: "https://warrantyit-app.netlify.app",
     credentials: true,
   })
 );
